0b9a96636894bc34aaf5fe889a4d2bae209c79da
liangqiseu <liangqiseu@163.com>
2019-02-27 00:00:35 +0800

    add echarts

--- flask_src/__init__.py
+++ flask_src/__init__.py
@@ -30,13 +30,6 @@ def create_app(test_config=None):
     from . import search
     app.register_blueprint(search.bp)    
     
-    from . import auth
-    app.register_blueprint(auth.bp)
-    
-    from . import blog
-    app.register_blueprint(blog.bp)
-    app.add_url_rule('/', endpoint='index')
-    
     return app
 
  
